c7568df570415f806bc917ed591e2fdee57e3825
Kemal Aditya <kadityafikar@gmail.com>
2019-02-15 18:20:00 +0700

    show table and count file

--- createSrt.js
+++ createSrt.js
@@ -1,18 +1,21 @@
 var fs = require('fs')
-var path = require('path')
 
 var path = __dirname
-fs.readdir(path,(err, items)=> {
+fs.readdir(path, (err, items) => {
+  var h = []
   loop:
     for (i of items) {
       if (!i.includes(".mp4")) {
         continue loop
       }
-      // console.log(i)
       var toSrt = i.replace(/.mp4/g, ".srt")
       var data = ""
+      h.push(toSrt);
+
       fs.writeFile(toSrt, data, (err) => {
-        err ? console.log(err) : console.log(`succes file di bikin ${toSrt}`);
+        err ? console.log(err) : "";
       })
-  }
+    }
+    console.table(h);
+    h.length === 0 ? console.log(`no mp4 file to create`) : console.log(`${h.length} has created`);
 });
