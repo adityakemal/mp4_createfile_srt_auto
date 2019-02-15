d36d6fa7a935f543017eeeedcdbacf3efce147d6
Kemal Aditya <kadityafikar@gmail.com>
2019-02-15 08:59:04 +0700

    oke

---
+++ createSrt.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+var path = require('path')
+
+var path = __dirname
+fs.readdir(path, function(err, items) {
+    loop:
+    for (i of items) {
+        if (!i.includes(".mp4")){
+          continue loop
+        }
+        // console.log(i)
+        var toSrt = i.replace(/.mp4/g,".srt")
+        var data = ""
+        fs.writeFile(toSrt,data, (err)=>{
+           err ? console.log(err) : console.log(`succes file di bikin ${toSrt}`);
+         })
+    }
+});
